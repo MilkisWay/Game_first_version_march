5254887660e9a09c6412a3b57da431b14080b5e1
Milana Shadrina <milana611shadrina@gmail.com>
2022-03-17 01:51:44 +0300

    Blum

--- Game_first_version_march.py
+++ Game_first_version_march.py
@@ -1,19 +1,29 @@
 import sys
-import unit
-import pygame 
+import pygame
 
 #General setup
 pygame.init()
 clock=pygame.time.Clock()
 
 #Game screen
-screen=pygame.dispaly.set_mode((1920,1080))
+screen=pygame.display.set_mode((1000,1000))
 pygame.display.set_caption("Harry Potter")
+
+#loadimages
+background_img=pygame.image.load('background.png')#free
+background1_img=pygame.image.load('background1.png')#free
+background2_img=pygame.image.load('background2.png')#free
 #run=True?
 while True:
+
+    screen.blit(background_img,(0,0))
+    screen.blit(background1_img,(0,0))
+    screen.blit(background2_img,(0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    pygame.display.flip()
+
+    pygame.display.update()
     clock.tick(60)
---
+++ player.py
@@ -0,0 +1,31 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.image = pygame.Surface((64,64))
+        self.image.fill('blue')
+        self.rect = self.image,get_rect(topleft=pos)
+        self.direction=pygame.math.Vector2(0,0)
+        self.speed = 10
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x =1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x =-1
+        elif keys[pygame.K_UP]:
+           self.direction.y =1
+        elif keys[pygame.K_DOWN]:
+            self.direction.x =-1
+        else:
+            self.direction.x =0
+            self.direction.y =0
+
+    def update(self):
+        self.get_input()
+        self.rect.x+=self.direction.x*self.speed
+        self.rect.y+=self.direction.y*self.speed
